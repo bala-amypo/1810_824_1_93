7e7b70e64417edbe253a0943eefba0c0f7d6fb54
bala-amypo <bala@amypo.in>
2025-12-17 10:31:41 +0000

    User commit

--- src/main/java/com/example/demo/Entityclass/ActiveIngredient.java
+++ src/main/java/com/example/demo/Entityclass/ActiveIngredient.java
@@ -2,6 +2,9 @@ package com.example.demo.Entityclass;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 public class ActiveIngredient()
 {
@@ -10,4 +13,31 @@ public class ActiveIngredient()
     private Long id;
     @Column(unique=true)
     private String name;
+
+   public Long getId()
+   {
+    return id;
+   }
+   public void  setId(Long id)
+   {
+    this.id=id;
+   }
+   public String getName()
+   {
+    return name;
+   }
+   public void setName(String name)
+   [
+    this.name=name;
+   ]
+   public ActiveIngredient(Long id,String name)
+   {
+    this.id=id;
+    this.name=name;
+   }
+   public ActiveIngredient()
+   {
+    
+   }
+
 }
