c38b4f86daacb3b575d775330517bb6f605d9d98
bala-amypo <bala@amypo.in>
2025-12-18 17:26:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework
+
 @RestController
 public class UserController{
     @Autowired
     UserService crs;
     @PostMapping("/rules")
-    public User rules
+    public User rules(@RequestBody User rule)
+    {
+        return crs.addRule(rule);
+    }
+    @GetMapping
+    public List<User> getrules()
+    {
+        return crs.getAllRules;
+    }
 
 }
