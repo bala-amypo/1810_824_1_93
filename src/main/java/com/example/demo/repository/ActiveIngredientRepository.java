c05fbff772389acbbb0cea1bffdc59b2ce4dda37
bala-amypo <bala@amypo.in>
2025-12-18 08:43:41 +0000

    User commit

--- src/main/java/com/example/demo/repository/ActiveIngredientRepository.java
+++ src/main/java/com/example/demo/repository/ActiveIngredientRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
-
+import com.example.demo.Entityclass.ActiveIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActiveIngredientRepository extends JpaRepository<ActiveIngredient,Long>()
 {
