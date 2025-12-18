b5aeda5af409f9a2990411284b7dfdf83191f2e0
bala-amypo <bala@amypo.in>
2025-12-18 09:04:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/InteractionRuleRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import 
+
+public interface InteractionRuleRepository extends JpaRepository<InteractionRule,Long>()
+{
+
+}
