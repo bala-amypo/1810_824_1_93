a6bc3f0f8e09bae8d6df55af1c346f22dac9bb43
bala-amypo <bala@amypo.in>
2025-12-18 06:41:17 +0000

    User commit

--- src/main/java/com/example/demo/Entityclass/InteractionRule.java
+++ src/main/java/com/example/demo/Entityclass/InteractionRule.java
@@ -9,13 +9,29 @@ public class InteractionRule{
     private String severity;
     private String description;
     
- public Long getId()
+   public Long getId()
    {
     return id;
    }
    public void  setId(Long id)
    {
     this.id=id;
+   }
+   public String getIngredientA()
+   {
+    return ingredientA;
+   }
+   public void setName(String ingredientA)
+   {
+    this.ingredientA=ingredientA;
+   }
+   public String getName()
+   {
+    return name;
+   }
+   public void setName(String name)
+   {
+    this.name=name;
    }
     
 }
