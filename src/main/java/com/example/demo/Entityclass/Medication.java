538de92a2c8ffde48144d0c73e3b04b9604761a1
bala-amypo <bala@amypo.in>
2025-12-18 06:21:18 +0000

    User commit

--- src/main/java/com/example/demo/Entityclass/Medication.java
+++ src/main/java/com/example/demo/Entityclass/Medication.java
@@ -8,4 +8,30 @@ public class Medication
     private String name;
     private String ingredients;
 
+    public Long getId()
+    {
+        return id;
+    }
+    public void setId(Long id)
+    {
+        this.id=id;
+    }
+    public String getName()
+   {
+        return name;
+   }
+    public void setName(String name)
+   {
+        this.name=name;
+   }
+    public String getIngredients()
+   {
+    return ingredients;
+   }
+   public void set(String name)
+   {
+    this.ingredients=ingredients;
+   }
+
+
 }
