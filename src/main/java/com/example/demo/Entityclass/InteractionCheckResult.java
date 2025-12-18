42e10cdfd05265335ebbf6abd842f7c150f4f809
bala-amypo <bala@amypo.in>
2025-12-18 07:53:25 +0000

    User commit

--- src/main/java/com/example/demo/Entityclass/InteractionCheckResult.java
+++ src/main/java/com/example/demo/Entityclass/InteractionCheckResult.java
@@ -7,5 +7,21 @@ public class InteractionCheckResult
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    
+    private String medications;
+    private LocalDateTime checkedAt;
+
+   public Long getId()
+   {
+    return id;
+   }
+   public void  setId(Long id)
+   {
+    this.id=id;
+   }
+   public String getMedications()
+   {
+    return medications;
+   }
+   p
+
 }
