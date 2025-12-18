05268c60b939810687841ca81472dcb25027ab6f
bala-amypo <bala@amypo.in>
2025-12-18 06:36:17 +0000

    User commit

--- src/main/java/com/example/demo/Entityclass/InteractionRule.java
+++ src/main/java/com/example/demo/Entityclass/InteractionRule.java
@@ -1,6 +1,21 @@
 package com.example.demo.Entityclass;
 
 public class InteractionRule{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String ingredientA;
+    private String ingredientB;
+    private String severity;
+    private String description;
     
+ public Long getId()
+   {
+    return id;
+   }
+   public void  setId(Long id)
+   {
+    this.id=id;
+   }
     
 }
