8df2cc134f15e353ec770f72eb0329f7890e1e2d
bala-amypo <bala@amypo.in>
2025-12-18 17:23:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+@RestController
 public class UserController{
-    
+    @Autowired
+    UserService crs;
+    @PostMapping("/rules")
+    public User rules
+
 }
