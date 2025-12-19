9a2d0a9be9bcffba570a3a2a18dcaf076e411b86
bala-amypo <bala@amypo.in>
2025-12-19 10:37:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.Entityclass.User;
 import com.example.demo.service.UserService;
 import java.util.List;
@@ -19,8 +20,8 @@ public class UserController{
     {
         return crs.addRule(rule);
     }
-    @GetMapping("{email}")
-    public User findemail(@PathVarialbString email)
+    @GetMapping("/{email}")
+    public User findemail(@PathVariable String email)
     {
         return crs.findByEmail(email);
     }
