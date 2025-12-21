4035abdf8e87060b456cffd2c3f77884239b8571
bala-amypo <bala@amypo.in>
2025-12-21 06:36:57 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/CatalogImp.java
+++ src/main/java/com/example/demo/service/implement/CatalogImp.java
@@ -4,4 +4,12 @@ import com.example.demo.repository.ActiveIngredientRepository;
 import com.example.demo.repository.MedicationRepository;
 import com.example.demo.Entityclass.Medication;
 import com.example.demo.Entityclass.ActiveIngredient;
+import com.example.demo.service.CatalogService;
+import 
 
+@Service
+public CatalogImp extends CatalogService{
+
+
+
+}
