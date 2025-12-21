9d83c8a49eb4260ede99dfd71ab3779d036dc15a
bala-amypo <bala@amypo.in>
2025-12-21 06:51:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/CatalogController.java
+++ src/main/java/com/example/demo/controller/CatalogController.java
@@ -1,3 +1,11 @@
 package com.example.demo.controller;
 
-import 
+import com.example.demo.service.CatalogService;
+import com.example.demo.Entityclass.ActiveIngredient;
+import com.example.demo.Entityclass.Medication;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class  CatalogController
+{
+
+} 
