6163eb3d7e61f115937ff78fcec5473e2da22d26
bala-amypo <bala@amypo.in>
2025-12-21 16:13:00 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/InteractionImp.java
+++ src/main/java/com/example/demo/service/implement/InteractionImp.java
@@ -1,4 +1,23 @@
 package com.example.demo.service.implement;
 
 import com.example.demo.Entityclass.InteractionCheckResult;
-import com.example.demo.repository.InteractionCheck
+import com.example.demo.repository.InteractionCheckResultRepository;
+import com.example.demo.service.InteractionService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class InteractionImp implements InteractionService{
+
+@Autowired
+InteractionCheckResultRepository rep;
+  public InteractionCheckResult checkInteractions(InteractionCheckResult medicationIds )
+  {
+  return rep.save(medicationIds);
+  }
+  public InteractionCheckResult getResult(Long resultId)
+  {
+    
+  }
+
+}
