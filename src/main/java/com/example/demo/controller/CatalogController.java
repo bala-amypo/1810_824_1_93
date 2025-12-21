341718722a8d89e305fefb2d9bf0ae3ad7dc09e5
bala-amypo <bala@amypo.in>
2025-12-21 06:54:57 +0000

    User commit

--- src/main/java/com/example/demo/controller/CatalogController.java
+++ src/main/java/com/example/demo/controller/CatalogController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVar
 import com.example.demo.service.CatalogService;
 import com.example.demo.Entityclass.ActiveIngredient;
 import com.example.demo.Entityclass.Medication;
