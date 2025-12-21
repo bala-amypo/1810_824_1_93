342dafba70535473b120bd00de2f3cc6f20e5565
bala-amypo <bala@amypo.in>
2025-12-21 08:55:26 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/RuleController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+@RestController
+public class RuleController{
+
+@Autowired
+RuleService run;
+
+@PostMapping("/inter")
+public InteractionRule addRule(Interaction Rule)
+{
+    return run.
+}
+
+}
