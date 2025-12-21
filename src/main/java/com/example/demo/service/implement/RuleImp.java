f3a2e5c94a55063133ec88739846d45cf5f1eb25
bala-amypo <bala@amypo.in>
2025-12-21 08:49:26 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/RuleImp.java
+++ src/main/java/com/example/demo/service/implement/RuleImp.java
@@ -1,10 +1,20 @@
 package com.example.demo.service.implement;
 
+import 
+
 @Service
 public class RuleImp implements RuleService{
 
 @Autowired
 InteractionRuleRepository rep;
 
-public InteractionRule addRule()
+public InteractionRule addRule(InteractionRule rule)
+{
+    return rep.save(rule);
+}
+
+public List<InteractionRule>getAllRules()
+{
+    return rep.findAll();
+}
 }
