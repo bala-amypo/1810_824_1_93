ec4d5b22094ce80f6f5aa42a38e4dcce399feb85
bala-amypo <bala@amypo.in>
2025-12-26 04:03:11 +0000

    User commit

--- src/main/java/com/example/demo/dto/RequestResponse.java
+++ src/main/java/com/example/demo/dto/RequestResponse.java
@@ -2,30 +2,40 @@ package com.example.demo.dto;
 
 public class RequestResponse {
 
-    private int status;
-    private String message;
+    private String name;
+    private String email;
+    private String password;
 
     public RequestResponse() {
     }
 
-    public RequestResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
+    public RequestResponse(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
-    public int getStatus() {
-        return status;
+    public String getName() {
+        return name;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getMessage() {
-        return message;
+    public String getEmail() {
+        return email;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
