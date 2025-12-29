a6b4900c69dbd318bd004c8bd96c20b33518a4fa
bala-amypo <bala@amypo.in>
2025-12-29 04:04:16 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,34 +1,9 @@
-package com.example.demo.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import java.util.List;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9014.32procr.amypo.ai/")
-                ));
-        }
- }
 // package com.example.demo.config;
 
 // import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.Components;
 // import io.swagger.v3.oas.models.servers.Server;
-// import io.swagger.v3.oas.models.security.SecurityScheme;
-// import io.swagger.v3.oas.models.security.SecurityRequirement;
-
 // import org.springframework.context.annotation.Bean;
 // import org.springframework.context.annotation.Configuration;
-
 // import java.util.List;
 
 // @Configuration
@@ -36,27 +11,52 @@ public class SwaggerConfig {
 
 //     @Bean
 //     public OpenAPI customOpenAPI() {
-
-        
-//         SecurityScheme bearerScheme = new SecurityScheme()
-//                 .type(SecurityScheme.Type.HTTP)
-//                 .scheme("bearer")
-//                 .bearerFormat("JWT");
-
 //         return new OpenAPI()
-                
+//                 // You need to change the port as per your server
 //                 .servers(List.of(
 //                         new Server().url("https://9014.32procr.amypo.ai/")
-//                 ))
+//                 ));
+//         }
+//  }
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                
+                .servers(List.of(
+                        new Server().url("https://9014.32procr.amypo.ai/")
+                ))
 
                
-//                 .components(new Components()
-//                         .addSecuritySchemes("BearerAuth", bearerScheme)
-//                 )
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", bearerScheme)
+                )
 
                
-//                 .addSecurityItem(new SecurityRequirement()
-//                         .addList("BearerAuth")
-//                 );
-//     }
-// }
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("BearerAuth")
+                );
+    }
+}
